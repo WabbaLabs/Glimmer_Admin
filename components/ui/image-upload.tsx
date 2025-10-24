dcdfb7f48cbcbcd9ce42e59075f1bb8edffb8fb5
WabbaLabs <ibragimirpost@gmail.com>
2025-10-25 01:55:30 +0300

    fix cloudinary

--- components/ui/image-upload.tsx
+++ components/ui/image-upload.tsx
@@ -49,7 +49,7 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
                     </div>
                 ))}
             </div>
-            <CldUploadWidget onUpload={onUpload} uploadPreset='xd1zgfvt'>
+            <CldUploadWidget onUpload={onUpload} uploadPreset='cloud_tool'>
                 {({ open }) => {
                     const onClick = () => {
                         open();
